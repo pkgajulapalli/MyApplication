f7ab24dfb6522f7e4df53f2119e48308208e4d78
Praveen Gajulapalli <praveenkuamr.gbvs@gmail.com>
2020-03-02 22:29:12 +0530

    enabled java 1.8 and data binding

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,26 +3,28 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     public static final String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(this::sendMessage);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.button.setOnClickListener(this::sendMessage);
     }
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
+    private void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = findViewById(R.id.editText);
-        String message = editText.getText().toString();
+        String message = binding.editText.getText().toString();;
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
